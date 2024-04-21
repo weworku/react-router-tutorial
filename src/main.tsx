03435a429ae075aa3e79f619f197cb41ad561a63
hidemaru4869 <hidehiko.saito.bbc4869@gmail.com>
2024-04-21 02:29:00 +0000

    連絡先コンポーネントをインポートし、新しいルートを作成する

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import {
 import './index.css'
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
@@ -14,10 +15,15 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
   },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
+
 )
